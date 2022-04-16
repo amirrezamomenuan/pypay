d111a228c8e53a1c9748026215a79751bf03ce78
amirrezamomenian <amirrezamomenian@gmail.com>
2022-04-16 18:04:21 +0430

    cleaned network.py

--- network.py
+++ network.py
@@ -1,6 +1,5 @@
 import json
 
-import requests
 from flask import (
     Flask,
     jsonify,
@@ -28,21 +27,18 @@ def initial_boot_data():
             "relative_nodes" : relative_nodes
         }
     )
-    # __________________ kind of done ____________________
 
 
 @app.route("/new-transaction", methods = ["POST"])
 def new_transaction():
     recieved_transaction = request.get_json()
-    # print(type(recieved_transaction))
-    # print("recieved transaction is : ",recieved_transaction)
+
     dictified_recieved_data = json.loads(recieved_transaction)
     response_data = transaction_core.validate_transaction(dictified_recieved_data)
 
     return jsonify(
         {"message" : response_data[0], "status" : response_data[1]}
     )
-    # __________________ kind of done ____________________
 
 
 @app.route("/new-block", methods = ["POST"])
@@ -50,9 +46,7 @@ def new_block():
     last_block = pypayd.deamon_node.get_last_block()
     recieved_block = request.get_json()
     response_data = block_core.handle_new_block(block = recieved_block, last_block=last_block)
-    # print("LAST BLOCK IS: ", last_block)
     if response_data[1] == CORRECT_STATUS_CODE:
-        # add block to blockchain
         print(response_data[0])
     else:
         print("AN ERROR OCCURED: ",response_data[0])
@@ -60,16 +54,6 @@ def new_block():
     return jsonify(
         {"message" : response_data[0], "status" : response_data[1]}
     )
-    # print("new block recieved")
-    # print(request.get_json())
-    # return jsonify(
-    #     {"message" : 'recieved', "status" : 200})
-    # __________________ kind of done ____________________
-
-
-@app.route("/get-full-chain", methods = ['GET'])
-def get_full_chain():
-    return "<p>Hello, World!</p>"
 
 
 @app.route("/get-mempool-transactions", methods = ['GET'])
